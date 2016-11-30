54804f7219e968aee7516766c180d6e329ff8dce
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-11-30 12:02:56 +0200

    up

---
+++ for/for/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region for tanim
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Merve Erpak");
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region whileFor
+
+            //int n = 5, fakto = 1;
+            //for (int i = 1; i <=n; i++)
+            //{
+            //    fakto *= i;
+            //}
+            //Console.WriteLine(fakto);
+            //Console.ReadLine();
+            #endregion
+
+            #region birden fazla kontrol
+            //bool durum = true;
+            //for (int i = 0; durum == true && i<100; i++)
+            //{
+            //    if (i * 2 > 20)
+            //    {
+            //        durum = false;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("{0} iken devam ederim", i);
+            //    }
+
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region breakContinue
+
+            //for (int i = 0; i < 100; i=i+5)
+            //{
+            //    if (i * 2 < 20)
+            //    {
+            //        //break; //döngüyü sonlandırır
+            //        /*continue; *///bir sonraki değere geçirir
+            //    }
+            //    Console.WriteLine("{0} iken devam ediyorum",i);
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region [1,n]aralığındaki sayıların toplamı
+            //int n = Convert.toInt32(Console.ReadLine());
+            //int toplam = 0;
+
+            //for (int i = 1; i < = n; i++)
+            //{
+
+            //}
+            #endregion
+
+            #region İcİceFor
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
+            #endregion
+
+
+        }
+    }
+}
