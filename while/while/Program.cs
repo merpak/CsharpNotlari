c35934674b720e7fe286a14d7aba6ae31336c865
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-11-30 10:10:28 +0200

    güncelleme

--- while/while/Program.cs
+++ while/while/Program.cs
@@ -35,28 +35,103 @@ namespace @while
             #endregion
 
             #region sayı bulma
-            Console.WriteLine("[1-10] arasında bir sayı tuttum, Tahmin edebilir misin?");
+            //Console.WriteLine("[1-10] arasında bir sayı tuttum, Tahmin edebilir misin?");
 
-            Random rnd = new Random();
-            int sayi = rnd.Next(1, 11);
-            int tahmin, counter=0;
+            //Random rnd = new Random();
+            //int sayi = rnd.Next(1, 11);
+            //int tahmin, counter=0;
 
-            while (true)
-            {
-                counter++;
-                Console.Write("Tahmin Et: ");
-                tahmin = Convert.ToInt32(Console.ReadLine());
-                if (tahmin == sayi)
-                {
-                    break;
-                }
-                                
-            }
-            Console.WriteLine("\nTebrikler... {0}. adımda bildin", counter);
-            Console.ReadKey();
+            //while (true)
+            //{
+            //    counter++;
+            //    Console.Write("Tahmin Et: ");
+            //    tahmin = Convert.ToInt32(Console.ReadLine());
+            //    if (tahmin == sayi)
+            //    {
+            //        break;
+            //    }
+
+            //}
+            //Console.WriteLine("\nTebrikler... {0}. adımda bildin", counter);
+            //Console.ReadKey();
             #endregion
 
+            #region devameden
 
+            //bool devam = true;
+            //while (devam)
+            //{
+            //    Console.Write("Birinci Sayı: ");
+            //    double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.Write("İkinci Sayı: ");
+            //    double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.Write("İslem Seç: ");
+            //    char islem = Convert.ToChar(Console.ReadLine());
+
+            //    double sonuc;
+
+            //    switch (islem)
+            //    {
+            //        case '+':
+            //            sonuc = sayi1 + sayi2;
+            //            break;
+
+            //        case '-':
+            //            sonuc = sayi1 - sayi2;
+            //            break;
+
+            //        case '*':
+            //            sonuc = sayi1 * sayi2;
+            //            break;
+
+            //        case '/':
+            //            sonuc = sayi1 / sayi2;
+            //            break;
+            //        default:
+            //            Console.WriteLine("Böyle bir şey yok!");
+            //            sonuc = 0;
+            //            break;
+
+
+            //    }
+
+            //    Console.WriteLine("Sonuc: " + sonuc);
+
+            //    Console.WriteLine("Devam Edilsin mi? (evet)");
+            //    string cevap = Console.ReadLine();
+
+            //    if (cevap == "evet")
+            //    {
+            //        devam = true;
+
+            //    }
+            //    else
+            //    {
+            //        devam = false;
+            //    }
+            //}
+            //Console.WriteLine("C u!");
+            //Console.ReadLine();
+            #endregion
+
+            #region taban hesaplama
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int sonuc = 0;
+            int basamak = 1;
+            while (sayi>0)
+            {
+                sonuc += (sayi % 2) * basamak;
+                basamak *= 10;
+                sayi /= 2;
+
+            }
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+            #endregion
+            
         }
     }
 }
