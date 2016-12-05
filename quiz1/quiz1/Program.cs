960853605dc360dee6d2956c24cc7ee32198b07a
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-12-05 10:13:08 +0200

    update

--- quiz1/quiz1/Program.cs
+++ quiz1/quiz1/Program.cs
@@ -10,7 +10,63 @@ namespace quiz1
     {
         static void Main(string[] args)
         {
+            //int n = 10;
+            //int toplam = 0, sayac = 1;
 
+            //while (sayac <= n)
+            //{
+            //    toplam += sayac;
+            //    sayac++;
+
+            //}
+            //Console.WriteLine(toplam);
+            //Console.Write("Tersten: ");
+            //while (n > 0)
+            //{
+            //    Console.Write(n + " ");
+            //    n--;
+
+            //}
+
+            //for (int i = 10; i > 0; i--) ;
+            //{
+            //    Console.Write(int + " ");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        if (i == 0 || i == 4 || j == 0 || j == 4)
+            //        {
+            //            Console.Write("* ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("  ");
+            //        }
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 8; j++)
+            //    {
+            //        if (i==0||i==4||j==0||j==7)
+            //        {
+            //            Console.Write("* ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("  ");
+            //        }
+                    
+            //    }
+            //    Console.WriteLine();
+            //}
         }
+
     }
 }
