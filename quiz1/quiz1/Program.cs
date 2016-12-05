6e89a48595458f9e17baff26f3e73f9eaba9ac7e
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-12-05 10:41:22 +0200

    update

--- quiz1/quiz1/Program.cs
+++ quiz1/quiz1/Program.cs
@@ -62,10 +62,44 @@ namespace quiz1
             //        {
             //            Console.Write("  ");
             //        }
-                    
+
             //    }
             //    Console.WriteLine();
             //}
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (i == 0 && j >= 2)
+                    {
+                        Console.Write("*");
+
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    if (i == 1 && (j == 1 || j == 4))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    if (i == 2 && j <= 3)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
         }
 
     }
