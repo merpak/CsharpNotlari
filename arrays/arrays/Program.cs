46bcad7d2aa2e57f65ccc5399cb70f8305ff727b
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-12-05 11:26:51 +0200

    update

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[3];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = i;
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+            //Console.ReadKey();
+
+            //Console.Write("Sınıf Mevcudu: ");
+            //int mevcut = Convert.ToInt32(Console.ReadLine());
+            //string[] ogrenciler = new string[mevcut];
+            //for (int i = 0; i < ogrenciler.Length; i++)
+            //{
+            //    Console.Write("{0}. öğrencinin ismi: ", i);
+            //    ogrenciler[i] = Console.ReadLine();
+
+            //}
+            //Console.Clear();
+            //Console.WriteLine("Ogrenciler: \n");
+            //foreach (var item in ogrenciler)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.ReadKey();
+            //{
+
+            //}
+
+
+
+        }
+
+
+    }
+}
