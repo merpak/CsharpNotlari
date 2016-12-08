410c8aece6c4a6386896d38f81cbb2a5327c7fdf
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-12-08 11:15:29 +0200

    update

---
+++ try_catch/try_catch/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    int[] array = new int[4];
+            //    Console.WriteLine(array[5]);
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());
+            //}
+            //catch (FormatException ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finally çalıştı.");
+            //}
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());       
+            //}
+            //catch (FormatException ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+
+            //}
+            //catch(OverflowException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("finally çalıştı.");
+            //}
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //try
+            //{
+            //    throw new IndexOutOfRangeException();
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+        }
+    }
+}
