528594d58271f54ae9c1607f9daa8e6da3409584
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-12-08 09:53:34 +0200

    update

---
+++ KalanCalisma/KalanCalisma/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KalanCalisma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OkBtn_Click(object sender, EventArgs e)
+        {
+            //int gunlukSaat = Convert.ToInt32(GunlukSaatTxt.Text);
+            //int gun = 10000 / gunlukSaat;
+            //int ay = gun / 30;
+            //int yil = gun / 365;
+            
+
+            //if (gunlukSaat > 24)
+            //{
+            //    MessageBox.Show("Yanlış değer!");
+            //    GunlukSaatTxt.Text = " ";
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Gün : " + gun + "\nAy : " + ay + "\nYıl : " + yil);
+            //}
+
+
+        }
+    }
+}
