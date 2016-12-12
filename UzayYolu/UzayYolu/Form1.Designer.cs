13898dfa678de02ab61c61d3ad78a9b45355d88b
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-12-12 11:54:32 +0200

    up

--- UzayYolu/UzayYolu/Form1.Designer.cs
+++ UzayYolu/UzayYolu/Form1.Designer.cs
@@ -28,13 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.spaceShip = new System.Windows.Forms.PictureBox();
+            this.rocket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.spaceShip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rocket)).BeginInit();
             this.SuspendLayout();
             // 
             // spaceShip
             // 
+            this.spaceShip.BackColor = System.Drawing.Color.Transparent;
             this.spaceShip.Image = ((System.Drawing.Image)(resources.GetObject("spaceShip.Image")));
             this.spaceShip.InitialImage = ((System.Drawing.Image)(resources.GetObject("spaceShip.InitialImage")));
             this.spaceShip.Location = new System.Drawing.Point(0, 0);
@@ -43,16 +48,36 @@
             this.spaceShip.TabIndex = 0;
             this.spaceShip.TabStop = false;
             // 
+            // rocket
+            // 
+            this.rocket.BackColor = System.Drawing.Color.Transparent;
+            this.rocket.Image = ((System.Drawing.Image)(resources.GetObject("rocket.Image")));
+            this.rocket.Location = new System.Drawing.Point(26, 184);
+            this.rocket.Name = "rocket";
+            this.rocket.Size = new System.Drawing.Size(16, 16);
+            this.rocket.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.rocket.TabIndex = 1;
+            this.rocket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(616, 362);
+            this.Controls.Add(this.rocket);
             this.Controls.Add(this.spaceShip);
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.spaceShip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rocket)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -60,6 +85,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox spaceShip;
+        private System.Windows.Forms.PictureBox rocket;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- UzayYolu/UzayYolu/Form1.cs
+++ UzayYolu/UzayYolu/Form1.cs
@@ -15,11 +15,18 @@ namespace UzayYolu
         public Form1()
         {
             InitializeComponent();
+            rocket.Visible = false;
+
         }
 
+        int genislik = Form1.ActiveForm.Width;
+        int yukseklik = Form1.ActiveForm.Height;
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            switch(e.KeyCode)
+            genislik = this.Width;
+            yukseklik = this.Height;
+
+            switch (e.KeyCode)
             {
                 case Keys.Left:
                     spaceShip.Left -= 10;
@@ -33,11 +40,50 @@ namespace UzayYolu
                 case Keys.Down:
                     spaceShip.Top += 10;
                     break;
+                case Keys.Space:
+                    int x = spaceShip.Location.X + spaceShip.Width / 2;
+                    int y = spaceShip.Location.Y + spaceShip.Height / 2;
+                    rocket.Location = new Point(x, y);
+                    rocket.Visible = true;
+                    break;
+
                 default:
                     break;
 
-                
             }
+
+            int SpaceX = spaceShip.Location.X;
+            int SpaceY = spaceShip.Location.Y;
+            if (SpaceX > genislik - spaceShip.Width || SpaceX < 0)
+            {
+                MessageBox.Show("Game Over!");
+                spaceShip.Location = new Point(10, 10);
+                //Application.Exit();
+
+            }
+            else if (SpaceY > yukseklik - spaceShip.Height || SpaceY < 0)
+            {
+                MessageBox.Show("Game Over!");
+                spaceShip.Location = new Point(10, 10);
+                //Application.Exit();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            genislik = this.Width;
+            yukseklik = this.Height;
+
+            if (rocket.Location.X < genislik - rocket.Width * 2)
+            {
+                rocket.Left += 10;
+            }
+            else
+            {
+                timer1.Stop();
+
+            }
+
         }
     }
 }
