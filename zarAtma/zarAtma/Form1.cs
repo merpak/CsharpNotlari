eb7fb915bee17c7d252975194670b51841c8af14
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-12-14 10:40:51 +0200

    up

---
+++ zarAtma/zarAtma/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zarAtma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ZarAt()
+        {
+            Random rnd = new Random();
+            int sayi = rnd.Next(1, 7);
+            pictureBox1.ImageLocation = System.IO.Path.GetFullPath("Images/" + sayi + ".png");
+            sayi = rnd.Next(1, 7);
+            pictureBox2.ImageLocation = System.IO.Path.GetFullPath("Images/" + sayi + ".png");
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ZarAt();
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            ZarAt();
+        }
+    }
+}
