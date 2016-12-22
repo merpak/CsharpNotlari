aac1bfb1f83455e7b9ffe86b303e9b535dd86086
merve.erpak <merve.erpak@ALS-YG39104-J06>
2016-12-22 10:35:41 +0200

    up

---
+++ Constructor/Constructor/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string yeniPozisyon = CBoxPozisyon.SelectedItem.ToString();
+            Oyuncu _oyuncu = new Oyuncu("Sehmuz");
+            MessageBox.Show(_oyuncu.adi);
+
+        }
+    }
+}
---
+++ Constructor/Constructor/Oyuncu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Oyuncu
+    {
+        public string adi;
+        public string takimi;
+        public byte formaNo;
+        public string pozisyon;
+
+        public Oyuncu(string ad)
+        {
+            adi = ad;   
+        }
+
+    }
+}
